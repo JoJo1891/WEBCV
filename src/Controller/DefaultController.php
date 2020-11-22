48018e006fb6b08dfe1be0600004418127e7e337
Dominique SYLVIUS <dominiquesylvius@AirdeDominique.lan>
2020-11-22 16:33:22 -0400

    add reset-password

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,13 +10,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DefaultController extends AbstractController
 {
 	/**
-    * @Route("/")
+    * @Route("/", name="app_home")
     */
     public function home(): Response
     {
         $number = random_int(0, 100);
 
         return $this->render('public/home.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+    /**
+    * @Route("/contact", name="app_contact")
+    */
+    public function contact(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('public/contact.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+    /**
+    * @Route("/about", name="app_about")
+    */
+    public function about(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('public/about.html.twig', [
             'number' => $number,
         ]);
     }
